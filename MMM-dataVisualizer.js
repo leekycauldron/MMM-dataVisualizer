bc7e965915661aed792792489f8ff7f173c67db1
Bryson <48774118+leekycauldron@users.noreply.github.com>
2021-12-24 16:53:21 -0500

    Add files via upload

---
+++ MMM-dataVisualizer.js
@@ -0,0 +1,92 @@
+Module.register("MMM-dataVisualizer", {
+
+    
+    defaults: {
+        link: "", // Link containing data.
+        data: [], // data to get and display. first one must be a number so that it can be visualized. Multiple values can be used.
+        thresholds: [], // Thresholds for color coding (INCLUSIVE). [min,max,color (rgb)] // This also creates the limits for the graph. If number surpasses thresholds., it will color code it as the color with the largest value.
+		refreshRate: 300000 
+},
+
+
+	getStyles: function() {
+		return [
+				"MMM-dataVisualizer.css",
+		];
+	},	
+
+
+	//Refreshes the module every x minutes.
+	start: function() {
+		var timeFormat = this.config.refreshRate;
+		console.log(`Starting Module: ${this.name}`)
+		var self = this;
+		setInterval( function(){
+			self.updateDom();
+		}, timeFormat);
+	},
+
+
+
+	getDom: function() {
+
+        this.numData;
+    	var wrapper = document.createElement("div");
+		wrapper.id = "wrapper";
+        
+
+        fetch(this.config.link)
+        .then(response => response.json())
+        .then(res => {
+            
+            let toDisplay = "";
+        
+            this.config.data.forEach(val => {
+                toDisplay += res[val];
+            });
+			
+            this.numData = res[this.config.data[0]];
+			this.max = this.config.thresholds[this.config.thresholds.length - 1][1]; // Gets the highest possible value
+
+            let visualization = document.createElement('canvas');
+	
+			let c = visualization.getContext("2d");
+			
+			// Place bar right in the middle
+			let pct = this.numData / this.max;
+			
+			let px =Math.round( pct * (toDisplay.length*20-1));
+
+			this.config.thresholds.forEach(threshold => {
+				if (this.numData >= threshold[0] && this.numData <= threshold[1]) {
+					c.strokeStyle = threshold[2];
+					c.fillStyle = threshold[2];
+				}
+			})
+
+			// Fill bar using thresholds.
+			c.rect((visualization.width/2)-((toDisplay.length*20)/2)+1,11,px,19);
+			c.fill();
+			c.strokeStyle = 'white';
+			c.rect((visualization.width/2)-((toDisplay.length*20)/2),10,toDisplay.length*20,20);
+			c.stroke();
+			
+
+			c.fillStyle = 'white';
+			c.font = '24px Sans-Serif';
+			c.fillText(toDisplay, (visualization.width/2)-(c.measureText(toDisplay).width/2), 60);
+
+			
+            wrapper.appendChild(visualization);
+		
+
+			
+
+        })
+        .catch(e => Log.error(e))
+
+		return wrapper;
+		
+	}
+
+});
